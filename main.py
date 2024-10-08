48eaa243f0f63d4a40a66236a27e8ceb5d9a62ec
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-10-08 18:22:57 +0100

    feat: Add functionality to display temp/humidity

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 import dht
 import machine
 import time
+import ssd1306
 
-d = dht.DHT11(machine.Pin(4))
-d = dht.DHT22(machine.Pin(4))
+d = dht.DHT22(machine.Pin(2))
 
-red = machine.Pin(3, machine.Pin.OUT)
-green = machine.Pin(2, machine.Pin.OUT)
-white = machine.Pin(1, machine.Pin.OUT)
+red = machine.Pin(5, machine.Pin.OUT)
+yellow = machine.Pin(21, machine.Pin.OUT)
+green = machine.Pin(0, machine.Pin.OUT)
 
 prev_led = None
 
+class Display():
+    def __init__(self):
+        # Initialize I2C with default pins (SDA=Pin(4), SCL=Pin(5))
+        i2c = machine.I2C(sda=machine.Pin(1), scl=machine.Pin(0))
+
+        # Initialize the OLED display
+        self.display = ssd1306.SSD1306_I2C(128, 64, i2c)
+
+        # Clear the display
+        self.display.fill(0)
+        self.display.show()
+    
+    def add_text(self, text, y):
+        start_x = int((128 - len(text) * 24) / 2)
+    
+        if start_x < 0:
+            start_x = 0
+        self.display.write_text(text, start_x, y, size=3)
+        
+    def reset(self):
+        self.display.fill(0)
+    
+    def show_text(self, text, y):
+        self.reset()
+        self.add_text(text, y)
+        self.show()
+        
+    def show(self):
+        self.display.show()
+        
 
 def change_led_color(led):
     global prev_led
@@ -24,10 +54,13 @@ def change_led_color(led):
         red.value(1)
     elif led is green:
         green.value(1)
-    elif led is white:
-        white.value(1)
+    elif led is yellow:
+        yellow.value(1)
 
     prev_led = led
+
+
+display = Display()
 
 
 while True:
@@ -35,13 +68,19 @@ while True:
     temp = d.temperature()
     humidity = d.humidity()
 
-    print(f"Temperature: {temp}°C, Humidity: {humidity}%")
+    output = f"Temperature: {temp}°C, Humidity: {humidity}%"
+
+    print(output)
+    display.reset()
+    display.add_text(f"{temp}c", 7)
+    display.add_text(f"{humidity}%", 41)
+    display.show()
 
     if (humidity >= 70):
         change_led_color(green)
     elif (humidity >= 60):
-        change_led_color(white)
+        change_led_color(yellow)
     elif (humidity < 60):
         change_led_color(red)
 
-    time.sleep(5)
+    time.sleep(1)
